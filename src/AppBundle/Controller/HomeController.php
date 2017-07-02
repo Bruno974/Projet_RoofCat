399bf76cbefcb589a4048908c347cdf3d6a043b0
bruno974 <gont.bruno@gmail.com>
2017-07-02 10:12:25 +0200

    Début mise en place de la page d'accueil + css + responsive

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bruno
+ * Date: 01/07/2017
+ * Time: 19:03
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function indexAction()
+    {
+        /*$annonceDerniere = $this->getDoctrine()->getManager()->getRepository('AppBundle:Annonce')->findDerniereAnnonce();
+        $annoncesRecentes = $this->getDoctrine()->getManager()->getRepository('AppBundle:Annonce')->findAnnoncesRecentes();*/
+        return $this->render('Accueil/accueil.html.twig');//, array('annoncesRecentes' => $annoncesRecentes, 'annonceDerniere' => $annonceDerniere));
+    }
+}
